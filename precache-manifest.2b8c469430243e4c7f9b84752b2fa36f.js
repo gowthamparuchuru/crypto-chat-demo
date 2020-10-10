b0833002d142d851705a6ac0e51ee0707f098c14
Paruchuru Venkata Sai Gowtham <pvsg737@gmail.com>
2020-10-10 17:09:55 +0530

    Updates

---
+++ precache-manifest.2b8c469430243e4c7f9b84752b2fa36f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c3611ed8eef716d212e8cea56d8159",
+    "url": "/crypto-chat-demo/index.html"
+  },
+  {
+    "revision": "828fce5afb5d7753a7cf",
+    "url": "/crypto-chat-demo/static/css/main.029563ab.chunk.css"
+  },
+  {
+    "revision": "84c54cc2be6523788714",
+    "url": "/crypto-chat-demo/static/js/2.226f6f68.chunk.js"
+  },
+  {
+    "revision": "0ee60463ad81b6fbb33914e8174955a9",
+    "url": "/crypto-chat-demo/static/js/2.226f6f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828fce5afb5d7753a7cf",
+    "url": "/crypto-chat-demo/static/js/main.f82c54ab.chunk.js"
+  },
+  {
+    "revision": "8fb96cd3e7bd4bfdc276",
+    "url": "/crypto-chat-demo/static/js/runtime-main.800731aa.js"
+  },
+  {
+    "revision": "af68e760865faaa32f8d186416c3ee19",
+    "url": "/crypto-chat-demo/static/media/googleIcon.af68e760.svg"
+  },
+  {
+    "revision": "14f8dffe8f927d11643bf9785c902ace",
+    "url": "/crypto-chat-demo/static/media/sendIcon.14f8dffe.svg"
+  }
+]);
